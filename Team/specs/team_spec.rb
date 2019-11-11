97b454b0e5bb15c6d231bbd6992bac7815618bcd
Ellie <cassona08@gmail.com>
2019-11-11 17:53:55 +0000

    adds the mvp

--- Team/specs/team_spec.rb
+++ Team/specs/team_spec.rb
@@ -32,10 +32,50 @@ end
 def test_set_coach
   players = ["Jane", "Sarah", "Sue"]
   team = Team.new("Ruby on Skates", players, "Coach Georgie")
-  team.set_coach("Coach Taz")
+  team.coach = "Coach Taz"
   assert_equal("Coach Taz", team.coach)
 end
 
+
+
+def test_add_new_player
+  players = ["Jane", "Sarah", "Sue"]
+  team = Team.new("Ruby on Skates", players, "Coach Georgie")
+  updated_players = ["Jane", "Sarah", "Sue", "Karen"]
+
+  team.add_new_player("Karen")
+  assert_equal(updated_players, team.players)
+
+end
+
+
+
+def test_checks_for_players_name__is_there
+  players = ["Jane", "Sarah", "Sue"]
+  team = Team.new("Ruby on Skates", players, "Coach Georgie")
+  assert_equal("Sue is there", team.checks_for_players_name("Sue"))
+
+end
+
+def test_checks_for_players_name__is_not_there
+  players = ["Jane", "Sarah", "Sue"]
+  team = Team.new("Ruby on Skates", players, "Coach Georgie")
+  assert_equal("Bob is not there", team.checks_for_players_name("Bob"))
+end
+
+def test_update_points__won
+  players = ["Jane", "Sarah", "Sue"]
+  team = Team.new("Ruby on Skates", players, "Coach Georgie")
+  team.update_points("Won")
+  assert_equal(1, team.points)
+end
+
+def test_update_points__lost
+  players = ["Jane", "Sarah", "Sue"]
+  team = Team.new("Ruby on Skates", players, "Coach Georgie")
+  team.update_points("Lost")
+  assert_equal(0, team.points)
+end
 
 ##
 end
--- Team/team.rb
+++ Team/team.rb
@@ -1,30 +1,34 @@
 class Team
+attr_reader :team_name, :players, :points
+attr_accessor :coach
 
 def initialize(input_team_name, input_players, input_coach)
 
   @team_name = input_team_name
   @players = input_players
   @coach = input_coach
+  @points = 0
 
 end
 
-def team_name
-  return @team_name
+def add_new_player(new_player)
+@players.push(new_player)
 end
 
-def players
-  return @players
+def checks_for_players_name(name)
+ for player in @players
+   if player == name
+     return "#{name} is there"
+   end
+ end
+ return "#{name} is not there"
 end
 
-def coach
-  return @coach
+def update_points(won_lost)
+  if won_lost == "Won"
+    @points += 1
+  end
 end
 
-def set_coach(new_coach)
-@coach = new_coach
 
 end
-
-##
-end
-##
