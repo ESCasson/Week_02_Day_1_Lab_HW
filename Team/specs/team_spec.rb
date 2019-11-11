1b41421308e99b13893f76b5cb67787a584afe0f
Ellie <cassona08@gmail.com>
2019-11-11 17:07:12 +0000

    adds part completed task b

--- Team/specs/team_spec.rb
+++ Team/specs/team_spec.rb
@@ -6,12 +6,35 @@ require_relative('../team.rb')
 class TestTeam < MiniTest::Test
 
 def test_can_create_team_object
-
-
+players = ["Jane", "Sarah", "Sue"]
+team = Team.new("Ruby on Skates", players, "Coach Georgie")
 end
 
+def test_team_name
+  players = ["Jane", "Sarah", "Sue"]
+  team = Team.new("Ruby on Skates", players, "Coach Georgie")
+  assert_equal("Ruby on Skates",team.team_name)
+end
 
+  def test_team_name
+    players = ["Jane", "Sarah", "Sue"]
+    team = Team.new("Ruby on Skates", players, "Coach Georgie")
+    assert_equal(players, team.players)
 
+  end
+
+def test_coach
+  players = ["Jane", "Sarah", "Sue"]
+  team = Team.new("Ruby on Skates", players, "Coach Georgie")
+  assert_equal("Coach Georgie", team.coach)
+end
+
+def test_set_coach
+  players = ["Jane", "Sarah", "Sue"]
+  team = Team.new("Ruby on Skates", players, "Coach Georgie")
+  team.set_coach("Coach Taz")
+  assert_equal("Coach Taz", team.coach)
+end
 
 
 ##
