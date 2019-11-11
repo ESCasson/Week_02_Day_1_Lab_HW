4c73088bc182c344fc85a813d5aa9f8a41def9f2
Ellie <cassona08@gmail.com>
2019-11-11 16:33:47 +0000

    adds files for Task B

---
+++ Team/specs/team_spec.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../team.rb')
+
+
+class TestTeam < MiniTest::Test
+
+def test_can_create_team_object
+
+
+end
+
+
+
+
+
+##
+end
+##
